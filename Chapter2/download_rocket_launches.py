bc06b90a8418e10e7b2cf9b7be6e6ce88c724b88
garden-jun <jeongwonjun0718@gmail.com>
2023-10-31 22:45:31 +0900

    fix : 스케쥴링 간격 : @daily

--- Chapter2/download_rocket_launches.py
+++ Chapter2/download_rocket_launches.py
@@ -10,14 +10,15 @@ from airflow.operators.python import PythonOperator
 dag=DAG( # 객체의 인스턴스 생성(구체화) - 모든 워크플로의 시작점. 워크플로 내의 모든 태스크는 DAG 개체를 참조하므로 Airflow는 어떤 태스크가 어떤 DAG에 속하는지 확인 할 수 있음.
     dag_id="download_rocket_launches", # DAG의 이름
     start_date=airflow.utils.dates.days_ago(14), # DAG 처음 실행 시작 날짜/시간 # 현재로부터 14일 전을 실행시작일로 지정하겠다는 의미. -> 과거데이터를 실행
-    schedule_interval=None, # DAG의 실행 간격 # None은 Dag가 자동으로 실행되지 않음을 의미함.
+    # schedule_interval=None, # DAG의 실행 간격 # None은 Dag가 자동으로 실행되지 않음을 의미함.
+    schedule_interval="@daily"
 )
 
 # 배시 커맨드를 실행하기 위해 BashOperator 객체 인스턴스 생성
 # 각 오퍼레이터는 하나의 태스크를 수행하고 여러 개의 오퍼레이터가 워크플로 또는 Dag를 구성. 
 download_launches = BashOperator(
     task_id="download_launches", # 태스크의 이름
-    bash_command="curl -o /tmp/launches.json 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", # 실행할 배시 커맨드
+    bash_command="curl -o /home/gardenjun/바탕화면/STUDY/airflow/Airflow/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", # 실행할 배시 커맨드
     dag = dag # DAG 변수에 대한 참조.
 )
 
@@ -46,16 +47,16 @@ download_launches = BashOperator(
 # PythonOperator를 사용한 파이썬 함수 실행
 def _get_pictures(): # 호출할 파이썬 함수
     # 디렉터리 확인
-    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True) # 디렉터리가 존재하지 않으면 생성
+    pathlib.Path("/home/gardenjun/바탕화면/STUDY/airflow/Airflow/images").mkdir(parents=True, exist_ok=True) # 디렉터리가 존재하지 않으면 생성
     # Download all pictures in launches.json
-    with open("/tmp/launches.json") as f: # 이전 단계의 테스크 결과 확인
+    with open("/home/gardenjun/바탕화면/STUDY/airflow/Airflow/launches.json") as f: # 이전 단계의 테스크 결과 확인
         launches=json.load(f) # 데이터를 섞을 수 있도록 딕셔너리로 읽기.
         image_urls = [launch["image"] for launch in launches["results"]]
         for image_url in image_urls:
             try:
                 response=requests.get(image_url) # 각각의 이미지 다운로드
                 image_filename=image_url.split("/")[-1]
-                target_file=f"/tmp/images/{image_filename}"
+                target_file=f"/home/gardenjun/바탕화면/STUDY/airflow/Airflow/images/{image_filename}"
                 with open(target_file, "wb") as f:
                     f.write(response.content) # 각각의 이미지 저장
                 print(f"Downloaded {image_url} to {target_file}") # Airflow 로그에 저장하기 위해 stdout으로 출력
